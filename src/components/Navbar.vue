0a2067f9786a63acde7e46f36f94ab352d9d909b
claudiobahia <claudio.bahia@al.infnet.edu.br>
2020-02-26 22:59:13 -0300

    skull

---
+++ src/components/Navbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <nav>
+    <v-app-bar app dark class="indigo">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Bem vindo!</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <span class="font-weight-light display-1">Locadora {{title}}</span>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" app dark class="indigo">
+      <v-list shaped>
+        <v-subheader>OPTIONS MENU</v-subheader>
+        <v-list-item-group>
+          <v-list-item v-for="(item, i) in items" :key="i" :to="item.route">
+            <v-list-item-icon>
+              <v-icon v-text="item.icon" />
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.text" />
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "API github de maluco",
+      drawer: false,
+      items: [
+        { text: "HOME", icon: "mdi-home", route:'/' },
+        { text: "CARRINHO", icon: "mdi-cart", route:'/cart' },
+        { text: "HISTÓRICO", icon: "mdi-history", route:'/history' }
+      ]
+    };
+  },
+  methods: {}
+};
+</script>
+
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Cart from '../views/Cart.vue'
+import History from '../views/History.vue'
+
 
 Vue.use(VueRouter)
 
@@ -11,12 +14,14 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/cart',
+    name: 'Cart',
+    component: Cart
+  },
+  {
+    path: '/history',
+    name: 'History',
+    component: History
   }
 ]
 
